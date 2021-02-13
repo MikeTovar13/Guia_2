77430ac79bdb5dd1dea4b5f92618283c3dbcab28
Tatiana Cuellar <tatianacuellar9923@gmail.com>
2021-02-13 11:20:15 -0500

    ejecicio 4

--- Guia_2/Guia_2/Exercise_4.cs
+++ Guia_2/Guia_2/Exercise_4.cs
@@ -5,7 +5,60 @@ using System.Text;
 namespace Guia_2 {
     class Exercise_4 {
         public void Ejecutar() {
-            Console.WriteLine("Programa 4, te hace X");
+            double ingreso= 0, incentivo = 0, salario = 0;
+            int producionSemanal;
+            Console.WriteLine("Programa 4, Te muestra si el operario recibe incentivos y cuanto es su salario semanal");
+            
+            Console.WriteLine("Ingrese la producción realizada durante la semana");
+            producionSemanal = int.Parse(Console.ReadLine());
+
+            if (producionSemanal < 99 )
+            {
+                // Ingreso 2 pesos
+                ingreso = producionSemanal * 2;
+                // Incentivo 0%
+                incentivo = (ingreso * 0) / 100;
+
+                Console.WriteLine("Tu producción semanal no son suficientes, la miníma es 99, no recibirás incentivos)");
+            }
+            else if (producionSemanal > 99 && producionSemanal < 199)
+            {
+                // Ingreso 2 pesos
+                ingreso = producionSemanal * 2;
+                // Incentivo 10%
+                incentivo = (ingreso * 10) / 100;
+            }
+            else if (producionSemanal > 200 && producionSemanal < 299)
+            {
+                // Ingreso 2,5 pesos
+                ingreso = producionSemanal * 2.5;
+                // Incentivo 12%
+                incentivo = (ingreso * 12) / 100;
+            }
+            else if (producionSemanal > 300 && producionSemanal < 399)
+            {
+                // Ingreso 3 pesos
+                ingreso = producionSemanal * 3;
+                // Incentivo 14%
+                incentivo = (ingreso * 14) / 100;
+            }
+            else if (producionSemanal > 400 && producionSemanal < 499)
+            {
+                // Ingreso 3,5 pesos
+                ingreso = producionSemanal * 3.5;
+                // Incentivo 16%
+                incentivo = (ingreso * 16) / 100;
+            }
+            else
+            {
+                Console.WriteLine("Superaste el máximo promedio de producción semanal");
+            }
+
+            Console.WriteLine("Tus incenitvos son: $" + incentivo);
+            Console.WriteLine("Tu ingreso es de: $" + ingreso);
+
+            salario = incentivo + ingreso;
+            Console.WriteLine("Tu salario de la semana es: $" + salario);
         }
     }
 }
