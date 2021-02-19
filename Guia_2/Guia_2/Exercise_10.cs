82e763ae447bd49117be6b74b5a7f2297ad5daa7
Daniela Espinosa Velez <daniela.espinosa@davincitech.co>
2021-02-19 18:04:34 -0500

    prigrama 8 y 9

--- Guia_2/Guia_2/Exercise_10.cs
+++ Guia_2/Guia_2/Exercise_10.cs
@@ -5,7 +5,19 @@ using System.Text;
 namespace Guia_2 {
     class Exercise_10 {
         public void Ejecutar() {
-            Console.WriteLine("Programa 10, te hace X");
+            Dictionary<string, Dictionary<string, object>> info_empleados = new Dictionary<string, Dictionary<string, object>>();
+            string repetir; 
+            do {
+
+                Console.WriteLine("Cálculo de nomina de empleados");
+                Console.WriteLine("Escriba la cantidad de empleados para caluclar la nomina");
+                int n_emp = int.Parse(Console.ReadLine());
+                info_empleados["emp1"] = new Dictionary<string, object>();
+                info_empleados["emp1"]["salario"] = 1000;
+                Console.WriteLine("Presiones enter para repetir, escriba NO para volver al menú");
+                repetir = Console.ReadLine();
+            } while (repetir != "no");
+        
         }
     }
 }
--- Guia_2/Guia_2/Exercise_8.cs
+++ Guia_2/Guia_2/Exercise_8.cs
@@ -8,7 +8,37 @@ namespace Guia_2
     {
         public void Ejecutar()
         {
-            Console.WriteLine("Programa 8, te hace X");
+            string continuar;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Cálculo de producción de gallinas");
+                Console.WriteLine("Ingrese la cantidad de gallinas");
+                int gallinas = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese la cantidad de días");
+                int dias = int.Parse(Console.ReadLine());
+ 
+                Console.WriteLine("La producción será de {0}", this.calcular_produccion(gallinas, dias));
+
+                Console.WriteLine("Para repetir presione entre, para salir escriba No");
+                continuar = Console.ReadLine().ToLower();
+
+            } while (continuar != "no");
+        }
+        private double calcular_produccion(int x, int n) {
+            double prod = 0;
+            for (int i = 0; i <= n; i++) {
+                prod = prod + Math.Pow(x, i) / this.fact(i);
+            }
+            return Math.Round(prod, 1);
+        
+        }
+        private double fact(int f) {
+            if (f < 2) {
+                return 1;
+            }
+            return f * this.fact(f - 1);
         }
     }
 }
