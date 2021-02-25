50b8ff4fb28a763d1f53156e6fa935e854c4dd02
Katerin Tatiana Cuellar Pardo <exttcp@gmv.es>
2021-02-24 22:57:33 -0500

    ejercicio 5

--- Guia_2/Guia_2/Exercise_4.cs
+++ Guia_2/Guia_2/Exercise_4.cs
@@ -3,62 +3,72 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Guia_2 {
-    class Exercise_4 {
+    class Exercise_4
+    {
+        string repetir;
         public void Ejecutar() {
-            double ingreso= 0, incentivo = 0, salario = 0;
-            int producionSemanal;
-            Console.WriteLine("Programa 4, Te muestra si el operario recibe incentivos y cuanto es su salario semanal");
-            
-            Console.WriteLine("Ingrese la producción realizada durante la semana");
-            producionSemanal = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.Clear();
+                double ingreso = 0, incentivo = 0, salario = 0;
+                int producionSemanal;
+                Console.WriteLine("Programa 4, Te muestra si el operario recibe incentivos y cuanto es su salario semanal");
 
-            if (producionSemanal < 99 )
-            {
-                // Ingreso 2 pesos
-                ingreso = producionSemanal * 2;
-                // Incentivo 0%
-                incentivo = (ingreso * 0) / 100;
+                Console.WriteLine("Ingrese la producción realizada durante la semana");
+                producionSemanal = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Tu producción semanal no son suficientes, la miníma es 99, no recibirás incentivos)");
-            }
-            else if (producionSemanal > 99 && producionSemanal < 199)
-            {
-                // Ingreso 2 pesos
-                ingreso = producionSemanal * 2;
-                // Incentivo 10%
-                incentivo = (ingreso * 10) / 100;
-            }
-            else if (producionSemanal > 200 && producionSemanal < 299)
-            {
-                // Ingreso 2,5 pesos
-                ingreso = producionSemanal * 2.5;
-                // Incentivo 12%
-                incentivo = (ingreso * 12) / 100;
-            }
-            else if (producionSemanal > 300 && producionSemanal < 399)
-            {
-                // Ingreso 3 pesos
-                ingreso = producionSemanal * 3;
-                // Incentivo 14%
-                incentivo = (ingreso * 14) / 100;
-            }
-            else if (producionSemanal > 400 && producionSemanal < 499)
-            {
-                // Ingreso 3,5 pesos
-                ingreso = producionSemanal * 3.5;
-                // Incentivo 16%
-                incentivo = (ingreso * 16) / 100;
-            }
-            else
-            {
-                Console.WriteLine("Superaste el máximo promedio de producción semanal");
-            }
+                if (producionSemanal < 99)
+                {
+                    // Ingreso 2 pesos
+                    ingreso = producionSemanal * 2;
+                    // Incentivo 0%
+                    incentivo = (ingreso * 0) / 100;
 
-            Console.WriteLine("Tus incenitvos son: $" + incentivo);
-            Console.WriteLine("Tu ingreso es de: $" + ingreso);
+                    Console.WriteLine("Tu producción semanal no son suficientes, la miníma es 99, no recibirás incentivos)");
+                }
+                else if (producionSemanal > 99 && producionSemanal < 199)
+                {
+                    // Ingreso 2 pesos
+                    ingreso = producionSemanal * 2;
+                    // Incentivo 10%
+                    incentivo = (ingreso * 10) / 100;
+                }
+                else if (producionSemanal > 200 && producionSemanal < 299)
+                {
+                    // Ingreso 2,5 pesos
+                    ingreso = producionSemanal * 2.5;
+                    // Incentivo 12%
+                    incentivo = (ingreso * 12) / 100;
+                }
+                else if (producionSemanal > 300 && producionSemanal < 399)
+                {
+                    // Ingreso 3 pesos
+                    ingreso = producionSemanal * 3;
+                    // Incentivo 14%
+                    incentivo = (ingreso * 14) / 100;
+                }
+                else if (producionSemanal > 400 && producionSemanal < 499)
+                {
+                    // Ingreso 3,5 pesos
+                    ingreso = producionSemanal * 3.5;
+                    // Incentivo 16%
+                    incentivo = (ingreso * 16) / 100;
+                }
+                else
+                {
+                    Console.WriteLine("Superaste el máximo promedio de producción semanal");
+                }
 
-            salario = incentivo + ingreso;
-            Console.WriteLine("Tu salario de la semana es: $" + salario);
+                Console.WriteLine("Tus incenitvos son: $" + incentivo);
+                Console.WriteLine("Tu ingreso es de: $" + ingreso);
+
+                salario = incentivo + ingreso;
+                Console.WriteLine("Tu salario de la semana es: $" + salario);
+
+                Console.WriteLine("\n Presiones enter para repetir, escriba NO para volver al menú");
+                repetir = Console.ReadLine();
+            } while (repetir != "no");
+
         }
     }
 }
--- Guia_2/Guia_2/Exercise_5.cs
+++ Guia_2/Guia_2/Exercise_5.cs
@@ -3,9 +3,54 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Guia_2 {
-    class Exercise_5 {
+    class Exercise_5
+    {
+        string repetir;
+        int nErrors = 0;
+        double tempMax= 0, tempMin= 0, med=0, max=0, min=0;
         public void Ejecutar() {
-            Console.WriteLine("Programa 5, te hace X");
+           do
+            {
+                Console.Clear();
+                Console.WriteLine("Programa 5, Calculos estación climática ");
+                int nDias = Utils.readInt("Ingrese la cantidad de días que vas a registrar las termperaturas: ");
+                double[][] temps = new double[2][];
+                for (int i = 0; i < nDias; i++)
+                {
+                    temps[i] = new double[2];
+                    Console.WriteLine("Temperatura del día {0}", i+1);
+                    tempMax = Utils.readDouble("Temperatura máxima: ");
+                    if (tempMax == 9)
+                    {
+                        tempMax = Utils.readDouble("Temperatura invalida, ingrese una temperatura máxima: ");
+                        nErrors++;
+                    }
+                    tempMin = Utils.readDouble("Temperatura minima: ");
+                    if (tempMin == 9) {
+                        tempMin = Utils.readDouble("Temperatura invalida, ingrese una temperatura minima: ");
+                        nErrors++;
+                    } else if (tempMin > tempMax)
+                    {
+                        tempMin = Utils.readDouble("La temperatura debe ser menor a la máxima, ingrese una temperatura minima: ");
+                    }
+                    temps[i][0] = tempMax;
+                    temps[i][1] = tempMin;
+                }
+
+                
+                Console.WriteLine("Temperatura máxima | Temperatura minima");
+                for (int ii = 0; ii < nDias; ii++)
+                {
+                    Console.WriteLine("{0} {1}", temps[ii][0], temps[ii][1]);
+                }
+                Console.WriteLine("Temperatura media: {0}", med);
+                Console.WriteLine("Temperatura máxima {0}", max);
+                Console.WriteLine("Temperatura minima {0}", min);
+                Console.WriteLine("Cantidad de errores: {0}", nErrors);
+
+                Console.WriteLine("\n Presiones enter para repetir, escriba NO para volver al menú");
+                repetir = Console.ReadLine();
+            } while (repetir != "no");
         }
     }
 }
